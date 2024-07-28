ab53f56a81380abffad3e3f8df064d1cff775248
himangi <himangitripathy@gmail.com>
2024-07-28 21:51:28 +0530

    string is null or not using apache commons dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        /*
-        ghj
-         */
-        System.out.println("hello world");
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str=sc.nextLine();
+        if(StringUtils.isBlank(str)){
+            System.out.println("string is blank");
+        }else{
+            System.out.println("String is not blank");
+        }
+        sc.close();
     }
 }
