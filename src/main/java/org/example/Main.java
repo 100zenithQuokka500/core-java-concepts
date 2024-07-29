20790cacae3b33a8d8b05d60f49fd08e4dc8df54
himangi <himangitripathy@gmail.com>
2024-07-29 16:25:43 +0530

    added mysql dependency and created jdbc connection using java jdbc code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,28 @@
 package org.example;
-
+import java.sql.*;
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        String url= "jdbc:mysql://localhost:3306/core_java" ;
+        String user="root";
+        String password="himangi";
+        Connection connection=null;
+        try{
+            connection=DriverManager.getConnection(url,user,password);
+            System.out.println("Connection to the database established successfully!");
+        } catch(SQLException e){
+            e.printStackTrace();
+        } finally {
+            if(connection!=null){
+                try{
+                    connection.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter a string:");
         String str=sc.nextLine();
