13cb84b40d1b4f3beae8fda8312368bb9d2fee9e
H1mang1 <himangi1tripathy@gmail.com>
2024-08-19 11:55:40 +0530

    task completed

--- src/main/java/org/example/CalculatorTest.java
+++ src/main/java/org/example/CalculatorTest.java
@@ -1,2 +1,10 @@
-package org.example;public class CalculatorTest {
+package org.example;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(2, 3);
+        assertEquals(5, result);
+    }
 }
