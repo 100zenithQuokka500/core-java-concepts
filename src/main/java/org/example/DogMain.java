abbb07a6d974cab6dc31f5c367b96bee0826c40d
H1mang1 <himangi1tripathy@gmail.com>
2024-08-20 10:46:34 +0530

    assignment completed

--- src/main/java/org/example/DogMain.java
+++ src/main/java/org/example/DogMain.java
@@ -3,7 +3,7 @@ package org.example;
 public class DogMain {
     public static void main(String[] args) {
         Dog dog=new Dog();
-        dog.makeSound();
+        dog.makeSound();gi
         dog.sleep();
     }
 
