db6131dc2c591a5692408be6f94c802fbbb9c7a5
Himangi Tripathy <123282807+H1mang1@users.noreply.github.com>
2024-08-20 10:50:08 +0530

    Update DogMain.java

--- src/main/java/org/example/DogMain.java
+++ src/main/java/org/example/DogMain.java
@@ -3,7 +3,7 @@ package org.example;
 public class DogMain {
     public static void main(String[] args) {
         Dog dog=new Dog();
-        dog.makeSound();gi
+        dog.makeSound();
         dog.sleep();
     }
 
